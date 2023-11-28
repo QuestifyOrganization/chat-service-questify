587d31e3e2dd3388d3ed6b6f3ab8b940c2462c8b
Deivid Hugo <deividhugoof@gmail.com>
2023-11-28 14:31:34 -0300

    Find Messages

--- app/services/socketService.js
+++ app/services/socketService.js
@@ -1,6 +1,6 @@
 const { Server } = require('socket.io');
 const MessageModel = require('../models/messageModel');
-const ChatUserModel = require('../models/chatGroupModel');
+const ChatUserModel = require('../models/chatUserModel');
 const authService = require('./authService');
 
 class SocketService {
@@ -19,7 +19,7 @@ class SocketService {
       this.setupChatGroupMembershipEvents(socket);
       this.setupMessageEvents(socket);
       this.setupMessageViewerEvents(socket);
-
+      this.setupChatUserEvents(socket);
       socket.on('disconnect', () => {
         console.log(`A client disconnected`);
 
@@ -38,6 +38,7 @@ class SocketService {
         const chatUser = await authService.findOrCreateUser(decodeduserToken);
         socket.chatUser = chatUser;
         this.userSocketMap[chatUser.id] = socket.id;
+        socket.emit('setMyUser', chatUser);
         console.log('A client connected with a valid token');
     } catch (error) {
         console.log('Invalid token, disconnecting client');
@@ -53,7 +54,6 @@ class SocketService {
 
   setupMessageEvents(socket) {
     socket.on('message', async (data) => {
-    
         try {    
           const newMessage = new MessageModel({
             messageText: data.messageText,
@@ -73,8 +73,46 @@ class SocketService {
           console.error('Error handling message event:', error.message);
         }
       });
+
+      socket.on('findMessages', async (targetUser) => {
+        try {    
+
+          const chatUserId = socket.chatUser.id;
+
+          const foundMessages = await MessageModel.find({
+            $or: [
+              { senderId: chatUserId,  recipientContentType: targetUser.recipientContentType, recipientObjectId: targetUser.recipientObjectId},
+              { senderId: targetUser.recipientObjectId, recipientContentType: 'ChatUser', recipientObjectId: chatUserId}
+            ]
+          });
+
+          socket.emit('findMessages', foundMessages);
+        } catch (error) {
+          console.error('Error handling message event:', error.message);
+        }
+      });
   }
 
+  setupChatUserEvents(socket) {
+    socket.on('findChatUsers', async (data) => {
+      try {    
+        const chatUsers = await ChatUserModel.find({
+          name: new RegExp(data.name, "i")
+        });
+  
+        const chatUsersWithOnlineStatus = chatUsers.map(user => {
+          const isOnline = this.userSocketMap[user._id] ? true : false;
+          return { ...user.toObject(), isOnline };
+        });
+  
+        socket.emit('findChatUsers', chatUsersWithOnlineStatus);
+      } catch (error) {
+        console.error('Error handling findChatUsers event:', error);
+      }
+    });
+  }
+  
+  
   setupMessageViewerEvents(socket) {
     socket.on('messageViewerEvent', (data) => {
       console.log('Received messageViewer event:', data);
